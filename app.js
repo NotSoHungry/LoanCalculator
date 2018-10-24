c3a2db148541cf1a7b7b1f2b01523c25282783fc
ALittleBitHungry <allittlebithungry93@gmail.com>
2018-10-25 01:58:45 +0200

    Add input validation, error message handling and remove toFixed from result calculation

--- app.js
+++ app.js
@@ -7,20 +7,37 @@ function calculate(e) {
   const loanAmount = Number(document.querySelector("#loan-amount").value),
         interest = Number(document.querySelector("#interest").value),
         yearsToRepay = Number(document.querySelector("#years-to-repay").value);
+	
+	if (loanAmount > 0 && interest >= 0 && yearsToRepay > 0) {
+		// Calculate monthly payment
+		const numberOfPayments = yearsToRepay * 12,
+					interestPerPayment = (interest / 100) / 12,
+					discountFactor = (Math.pow(1 + interestPerPayment, numberOfPayments) - 1) / (interestPerPayment * Math.pow(1 + interestPerPayment, numberOfPayments));
 
-  // Calculate monthly payment
-  const numberOfPayments = yearsToRepay * 12,
-        interestPerPayment = (interest / 100) / 12,
-        discountFactor = (Math.pow(1 + interestPerPayment, numberOfPayments) - 1) / (interestPerPayment * Math.pow(1 + interestPerPayment, numberOfPayments));
+		const monthlyPayment = (interest) ? loanAmount / discountFactor : loanAmount / numberOfPayments,
+					totalPayment = monthlyPayment * numberOfPayments,
+					totalInterest = totalPayment - loanAmount;
 
-  const monthlyPayment = (loanAmount / discountFactor).toFixed(2),
-        totalPayment = (monthlyPayment * numberOfPayments).toFixed(2),
-        totalInterest = (totalPayment - loanAmount).toFixed(2);
+		// Print results
+			document.querySelector("#monthly-payment").value = monthlyPayment;
+			document.querySelector("#total-payment").value = totalPayment;
+			document.querySelector("#total-interest").value = totalInterest;
+	} else {
+		// Create error div
+		let errorMessage = document.createElement('div'),
+				errorMessageText = document.createTextNode('Please check your numbers');
+		errorMessage.appendChild(errorMessageText);
+		errorMessage.classList = "alert alert-danger form-error-custom";
 
-  // Print results
-    document.querySelector("#monthly-payment").value = monthlyPayment;
-    document.querySelector("#total-payment").value = totalPayment;
-    document.querySelector("#total-interest").value = totalInterest;
+		// Add error message above the form
+		const loanForm = document.querySelector("#loan-form");
+		document.querySelector(".card-form").insertBefore(errorMessage, loanForm);
 
-  e.preventDefault();
-}
+		// Remove the error message after 3 seconds
+		window.setTimeout(() => {
+			errorMessage.remove();
+		}, 3000);
+	}
+	
+	e.preventDefault();
+};
