32b89d90ce4c65e09bc53c449ee7c8430864edf1
ALittleBitHungry <allittlebithungry93@gmail.com>
2018-11-18 02:06:55 +0100

    Continue with the calculate function to show loader when calculating results

--- app.js
+++ app.js
@@ -6,7 +6,52 @@ function calculate(e) {
   // Take loan data from the form
   const loanAmount = Number(document.querySelector("#loan-amount").value),
         interest = Number(document.querySelector("#interest").value),
-        yearsToRepay = Number(document.querySelector("#years-to-repay").value);
+				yearsToRepay = Number(document.querySelector("#years-to-repay").value);
+
+	// Get results card object
+	const resultsCard = document.querySelector('.card-results');
+	
+	// Function for generating the loader's html markup
+	const generateResultsLoader = function() {
+		return `
+			<div class="card-results__loader">
+				<img src="./img/loading.gif" alt="Loading">
+			</div>
+		`
+	};
+
+	// Function for generating the result's html markup
+	const generateResults = function(monthlyPayment, totalPayment, totalInterest) {
+		return `
+			<div class="form-group">
+				<div class="input-group">
+						<span class="input-group-addon">Monthly Payment</span>
+						<input type="number" class="form-control" id="monthly-payment" value=${monthlyPayment} disabled>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="input-group">
+						<span class="input-group-addon">Total Payment</span>
+						<input type="number" class="form-control" id="total-payment" value=${totalPayment} disabled>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="input-group">
+						<span class="input-group-addon">Total Interest</span>
+						<input type="number" class="form-control" id="total-interest" value=${totalInterest} disabled>
+				</div>
+			</div>
+		`
+	}
+
+	// Expand the results card and show loader icon
+	resultsCard.innerHTML = generateResultsLoader();
+	resultsCard.classList.toggle('show-results');
+	setTimeout(function() {
+		resultsCard.firstElementChild.classList.toggle('show');
+	}, 10);
+	
+
 	
 	if (loanAmount > 0 && interest >= 0 && yearsToRepay > 0) {
 		// Calculate monthly payment
@@ -23,20 +68,28 @@ function calculate(e) {
 			document.querySelector("#total-payment").value = totalPayment.toFixed(2);
 			document.querySelector("#total-interest").value = totalInterest.toFixed(2);
 	} else {
-		// Create error div
-		let errorMessage = document.createElement('div'),
-				errorMessageText = document.createTextNode('Please check your numbers');
-		errorMessage.appendChild(errorMessageText);
-		errorMessage.classList = "alert alert-danger form-error-custom";
+		setTimeout(function() {
+			// Create error div
+			let errorMessage = document.createElement('div'),
+			errorMessageText = document.createTextNode('Please check your numbers');
+			errorMessage.appendChild(errorMessageText);
+			errorMessage.classList = "alert alert-danger form-error-custom";
 
-		// Add error message above the form
-		const loanFormHeader = document.querySelector(".card-form h1");
-		document.querySelector(".card-form").insertBefore(errorMessage, loanFormHeader);
+			// Add error message above the form
+			const loanFormHeader = document.querySelector(".card-form h1");
+			document.querySelector(".card-form").insertBefore(errorMessage, loanFormHeader);
 
-		// Remove the error message after 3 seconds
-		window.setTimeout(() => {
-			errorMessage.remove();
-		}, 2000);
+			// Remove the loader and set the hight of result card to 0
+			resultsCard.style.height = "0";
+			resultsCard.classList.toggle('show-results');
+			resultsCard.firstElementChild.remove();
+			resultsCard.style.height = "";
+
+			// Remove the error message after 3 seconds
+			window.setTimeout(() => {
+				errorMessage.remove();
+			}, 2000);
+		}, 700)
 	}
 	
 	e.preventDefault();
